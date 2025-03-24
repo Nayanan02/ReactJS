850530954214f47c4a600a9c804f86ed09c4fa8f
nayana02 <nayanaachar1912@gmail.com>
2025-03-24 23:22:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      
+        <title>Home | My React App</title>
+        <meta name="description" content="This is the homepage of my React app." />
+        <meta name="keywords" content="React" />
+      
+      <h1>Welcome to My React App</h1>
+    </div>
+  );
+}
+
+export default App;
