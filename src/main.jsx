9575839467c7e7b0388d2b97c89bf41f3f37c078
nayana02 <nayanaachar1912@gmail.com>
2025-03-25 15:27:04 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import FavoriteColor from './usestate.jsx';  
+import FavoriteColor from './Component/usestate.jsx'; 
+import Timer from './Component/useeffect.jsx';  
+
 // import Car from './props.jsx';  
-import Brand from './props.jsx';
-import CounterFunction from './FuncComponent.jsx'
-import CounterClass from './Classcomponent.jsx'
+import Brand from './Component/props.jsx';
+import CounterFunction from './Component/FuncComponent.jsx'
+import CounterClass from './Component/Classcomponent.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <FavoriteColor/>
+    <Timer/>
     <Brand/>
     <CounterFunction/>
     <CounterClass/>
