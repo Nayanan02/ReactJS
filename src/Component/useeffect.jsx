54380796f0335fea739a804176029aea8c5f07ec
nayana02 <nayanaachar1912@gmail.com>
2025-03-25 15:29:18 +0530

    second commit

--- src/Component/useeffect.jsx
+++ src/Component/useeffect.jsx
@@ -7,7 +7,7 @@ function Timer(){
           setCount((count) => count + 1);
         }, 1000);
       }, []);
-      
+    //  
 return <h1>I've rendered {count} times</h1>;
 }
 export default Timer;
